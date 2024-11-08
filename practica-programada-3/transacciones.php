4ce6cc095aa8afd852832870902bef1540ead799
Alejandro Cerdas J <acerdas00019@ufide.ac.cr>
2024-11-07 21:55:40 -0600

    tarea

---
+++ practica-programada-3/transacciones.php
@@ -0,0 +1,20 @@
+<?php
+
+// arreglo transacciones
+
+$transacciones = [
+    ["id"=> 1,"descripcion"=> "seguro","monto"=> 20000],
+    ["id"=> 2,"descripcion"=> "taxi","monto"=> 7000],
+    ["id"=> 3,"descripcion"=> "alquiler","monto"=> 2500000],
+    ["id"=> 4,"descripcion"=> "carne","monto"=> 4000],
+    ["id"=> 5,"descripcion"=> "escuela","monto"=> 90000],
+    ["id"=> 6,"descripcion"=> "compras","monto"=> 45000],
+    ["id"=> 7,"descripcion"=> "moto","monto"=> 76000]
+
+];
+
+//funcion agragar transaccion
+function registrarTransaccion($id, $descripcion, $monto){
+    global $transacciones;
+    
+}
